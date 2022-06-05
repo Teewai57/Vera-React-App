366907330c56ddaf9dd2eadd4006ebecdbd51b70
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-05 22:30:15 +0100

    done with content

---
+++ src/content.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './content.css'
+
+
+export default function Content() {
+    return(
+        <div className="row mt-5">
+            <div className="col-md-6">
+                <div className="content">
+                    <h1>Design a better <br></br> website template </h1>
+                    <p className="w-70 mt-3">Giving you the best tools to work it to build better templates <br></br> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <button className="btn btn-primary pl-5 pr-5 pt-2 pb-2 mt-2">
+                    FREE DOWNLOAD
+                </button>
+                </div>
+            </div>
+            <div className="col-md-6">
+
+            </div>
+        </div>
+    )
+}
