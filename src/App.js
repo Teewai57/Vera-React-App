5e05681a8fbeac5e8c28e1cdc4b218fac6a96b85
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-05 22:00:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container-fluid ">
+      <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+        <p class="navbar-brand pl-1">Atomic</p>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <p class="nav-link" href="#">Home</p>
+            </li>
+            <li class="nav-item">
+              <p class="nav-link" href="#">About</p>
+            </li>
+            <li class="nav-item">
+              <p class="nav-link" href="#">Company</p>
+            </li>
+            <li class="nav-item">
+              <p class="nav-link" href="#">Dashboard</p>
+            </li>
+            <li class="nav-item">
+              <p class="nav-link" href="#">Contact Us</p>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
