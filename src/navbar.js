39cc25112d7439ab1944bf6c989d3934bcb01830
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-06 09:30:27 +0100

    added sticky-top

--- src/navbar.js
+++ src/navbar.js
@@ -3,7 +3,7 @@ import './navbar.css'
 
 export default function Navbar(){
     return(
-        <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+        <nav class="navbar navbar-expand-lg navbar-light justify-content-between sticky-top">
         <p class="navbar-brand">Atomic</p>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
