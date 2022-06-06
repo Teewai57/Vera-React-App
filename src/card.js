7f284622e566cd50d9eac2e99ce22c35d01e1999
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-06 01:25:03 +0100

    stylings

--- src/card.js
+++ src/card.js
@@ -3,40 +3,37 @@ import './card.css'
 
 export default function Card() {
     return (
-        <div className="row justify-content-around card-content">
+        <div className="row justify-content-around card-content mb-4 shadow">
             <div className="col-md-4">
                 <div className="card b-none outline-none">
                     <div className="card-body b-none outline-none">
-                        <h5 className="card-title">Card title</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <i class="fa fa-lightbulb-o icon mb-3" aria-hidden="true"></i>
+                        <h5 className="card-title">Intuitive Thinking</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
                     </div>
                 </div>
             </div>
             <div className="col-md-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <div className="card b-none outline-none">
+                    <div className="card-body b-none outline-none">
+                    <i class="fa fa-search-plus icon mb-3" aria-hidden="true"></i>
+
+                        <h5 className="card-title">Search Engines</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
                     </div>
                 </div>
             </div>
             <div className="col-md-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <div className="card b-none outline-none">
+                    <div className="card-body b-none outline-none">
+                    <i class="fa fa-spinner icon mb-3" aria-hidden="true"></i>
+
+                        <h5 className="card-title">Infinite Possibilities</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
--- src/content.js
+++ src/content.js
@@ -5,8 +5,8 @@ import Image from './images.png'
 
 export default function Content() {
     return(
-        <div className="row mt-5">
-            <div className="col-md-6 justify-self-center">
+        <div className="row mt-5 content-row">
+            <div className="col-md-6 justify-self-center first-column">
                 <div className="content">
                     <h1>Design a better <br></br> website template </h1>
                     <p className="w-70 mt-3">Giving you the best tools to work it to build better templates <br></br> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
@@ -15,9 +15,9 @@ export default function Content() {
                 </button>
                 </div>
             </div>
-            <div className="col-md-6 content-img justify-self-center">
-                <img src={Image} alt="Vera Image"></img>
-                <a href="https://www.youtube.com/watch?v=xfQItZABQBA&t=5s"><i class="fa fa-youtube-play play" aria-hidden="true"></i></a>
+            <div className="col-md-6 justify-self-center mt-3">
+                <img src={Image} alt="V"></img>
+                <a href="https://www.youtube.com/watch?v=xfQItZABQBA&t=5s"><i class="fa fa-youtube-play play" aria-hidden="true"></i>Watch</a>
             </div>
         </div>
     )
