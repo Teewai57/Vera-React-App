954b09bea80114f7caeba0e6e81b6c3b8db46218
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-08 11:59:30 +0100

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Navbar from './navbar'
 import Content from './content'
 import Card from './card'
+import Footer from './footer.js'
 import './App.css'
 
 function App() {
   return (
+    <div className='Container-fluid'>
       <div className='container'>
-            <Navbar />
-            <Content />
-            <Card />
+        <Navbar />
+        <Content />
+        <Card />
       </div>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+
+
+export default function Footer(){
+    return(
+        <div className='Footer'>
+            <div className='row justify-content-center'>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
